4ca6f8e998e7a1e3ab1d5f26290c069ec98c2b8f
Jodi-Ann Moore-Barrett <moorebarrett.jodiann@gmail.com>
2022-12-13 09:09:41 -0600

    Initialize project files

---
+++ src/scripts/index.js
@@ -0,0 +1,91 @@
+'use strict';
+
+/*--------------------------------------------------------------------------- *
+ * Find Me Geolocation Website
+ * Jodi-Ann Barrett
+ * 
+ * */
+
+// Utility Functions 
+function onEvent(event, selector, callback) {
+    return selector.addEventListener(event, callback);
+  }
+  
+// Select HTML element by class, id and html element
+function select(selector, parent = document) {  
+    return parent.querySelector(selector);
+}
+
+function selectAll(selector, parent = document) {  
+    return parent.querySelectorAll(selector);
+}
+
+/**-----------------------------------DATA------------------------------------ */
+
+// const longitudeCoords = select('.longitude');
+// const latitudeCoords = select('.latitude');
+
+// const dialog = select('dialog');
+// const aboutUs = select('.about');
+
+const overlay = select('.overlay');
+
+// Map interface handlers
+const scrollZoom = 'scrollZoom';
+const boxZoom = 'boxZoom';
+const doubleClickZoom = 'doubleClickZoom';
+const dragRotate = 'dragRotate';
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoiam9kaWFubmJhcnJldHQiLCJhIjoiY2xiZ3JxMzJmMGFjcDN2bW1ydjlpc2NjYyJ9.pgkAM_oUNu6TpYp8ScH9Ow';
+const options = {
+    enableHighAccuracy: true
+}
+/**--------------------------------------------------------------------------- */
+
+/**-----------------------------RENDER MAP------------------------------------ */
+
+function getLocation(position) {
+    const {longitude, latitude} = position.coords; 
+    // longitudeCoords.innerText = longitude;
+    // latitudeCoords.innerText = latitude;    
+    
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v12', // style URL
+        center: [longitude, latitude], // starting position [lng, lat]
+        zoom: 15, // starting zoom,
+        pitch: 60 // pitch in degrees
+    });
+    
+    overlay.style.display = 'none';
+    const watermark1 = select('.mapboxgl-ctrl-bottom-left');
+    const watermark2 = select('.mapboxgl-ctrl-bottom-right');
+    watermark1.style.display = 'none';
+    watermark2.style.display = 'none';
+
+    //disable scrollZoom handlers
+    map[scrollZoom].disable();
+    map[boxZoom].disable();
+    map[doubleClickZoom].disable();
+    map[dragRotate].enable();
+
+    // Add zoom and rotation controls to the map.
+    map.addControl(new mapboxgl.NavigationControl());
+    
+    // Default Marker and add it to the map.
+    const marker = new mapboxgl.Marker({ color: '#232ED1' })
+    .setLngLat([longitude, latitude])
+    .addTo(map);
+}
+
+function errorHandler(error) {
+    console.log(error.message);
+}
+
+if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(getLocation, errorHandler, options);
+} else {
+    console.log('Geolocation is not supported by your browser');
+}
+
+/**--------------------------------------------------------------------------- */
